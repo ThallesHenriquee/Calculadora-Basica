efc916e442d838b687ee8d837d4bec8bceb631f8
ThallesHenriquee <devthalles.h@gmail.com>
2024-07-04 11:13:41 -0300

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Calculator
+    {
+        public int Calcular(string op, int value1, int value2)
+        {
+            switch (op)
+            {
+                case "+":
+                    return value1 + value2;
+                case "-":
+                    return value1 - value2;
+                case "*":
+                    return value1 * value2;
+                case "/":
+                    return value1/value2;
+                default:return 0;
+            }
+        }    } }
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Teste;
+Console.WriteLine("Digite seus valores");
+var N1 = int.Parse(Console.ReadLine());
+var N2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o operador");
+var op = Console.ReadLine();
+Calculator calculator = new Calculator();
+calculator.Calcular(op,N1,N2);
+Console.WriteLine(calculator.Calcular(op, N1, N2));
